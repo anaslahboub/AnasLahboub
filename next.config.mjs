c1b8af7a630328cc78c816905f2ea259434f7bd4
anaslahboub <151738195+anaslahboub@users.noreply.github.com>
2025-06-26 18:06:13 +0100

    Add GitHub Actions deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -10,7 +11,9 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',  // Génère des fichiers HTML/JS statiques (obligatoire pour GitHub Pages)
+  trailingSlash: true,  // Important pour GitHub Pages
   basePath: process.env.NODE_ENV === 'production' ? '/AnasPort' : '',  // Important si ton repo n'est pas "tonuser.github.io"
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/AnasPort/' : '',  // Pour les assets
 }
 
 export default nextConfig
