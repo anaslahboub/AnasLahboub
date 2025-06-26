9de811698fe1b37a86599b7bbbffa49131361b73
anaslahboub <151738195+anaslahboub@users.noreply.github.com>
2025-06-26 20:07:47 +0100

    erreur dans image

--- next.config.js
+++ next.config.js
@@ -1,15 +1,33 @@
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true
+//   },
+//   // Remplacez 'AnasLahboub' par le nom exact de votre repository
+//   basePath: process.env.NODE_ENV === 'production' ? '/AnasLahboub' : '',
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/AnasLahboub/' : '',
+// }
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'AnasLahboub' // Remplacez par le nom exact de votre repo
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   trailingSlash: true,
   images: {
-    unoptimized: true
-  },
-  // Remplacez 'AnasLahboub' par le nom exact de votre repository
-  basePath: process.env.NODE_ENV === 'production' ? '/AnasLahboub' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/AnasLahboub/' : '',
+    unoptimized: true,
+    // Ajoutez cette configuration pour les images
+    loader: 'custom',
+    loaderFile: './imageLoader.js'
+  }
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
